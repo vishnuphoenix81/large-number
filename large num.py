6a20c20eab632bf965bbbcc17d192a899ad589f9
vishnuphoenix81 <43330715+vishnuphoenix81@users.noreply.github.com>
2019-02-20 14:37:47 +0530

    Update and rename README.md to large num.py

---
+++ large num.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if(a>b and a>c):
+    print("a is larger")
+elif(b>c):
+    print("b is larger")
+else:
+    print("c is larger")
